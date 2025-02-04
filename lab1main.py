e90d4b9773a60e812e280dbe9341fd32ae0e5916
JuanOrtiz-cep <est.juan.ortiz4@unimilitar.edu.co>
2025-02-03 21:38:40 -0500

    Update lab1main.py

--- lab1main.py
+++ lab1main.py
@@ -57,7 +57,7 @@ mp.grid()
 mp.show()
 
 # FUNCION DE PROBABILIDAD CON NUMPY
-#counts, bins = np.histogram(original_signal, bins=20) # Ajusta el número de bins según sea necesario
+#counts, bins = np.histogram(original_signal, bins=20) 
 
 # FUNCION DE PROBABILIDAD SIN NUMPY
 min_val = min(original_signal)
@@ -236,7 +236,7 @@ device_signal = original_signal + device_noise
 mp.figure(figsize=(12,4))
 mp.plot(time_10s, device_signal[:num_muestras_10s], label='señal ruido artefacto')
 mp.grid()
-mp.title('señal ruido artefacto (cu01)')
+mp.title('señal ruido artefacto 2(cu01)')
 mp.xlabel('Tiempo[s]')
 mp.ylabel('Amplitud[mV]')
 mp.show()
@@ -246,4 +246,4 @@ cuadrado_ruido_artefacto = np.mean(device_noise**2)
 
 # Calcula el SNR en decibeles (dB)
 snr = 10 * np.log10(cuadrado_osignal / cuadrado_ruido_artefacto)
-print(f"SNR Artefacto: {snr} dB")
+print(f"SNR Artefacto 2: {snr} dB")
